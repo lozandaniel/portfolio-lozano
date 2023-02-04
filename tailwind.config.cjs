73cfb4c4c95427fe09d274b55b18c38f853fb7d5
Daniel Lozano <dl066522@gmail.com>
2023-02-03 22:29:19 -0500

    Add new style of the box-shadow property

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'3xl': '0 0 0 1px rgb(0 0 0 / 7%), 0 2px 4px rgb(0 0 0 / 5%), 0 12px 24px rgb(0 0 0 / 5%)',
+			}
+		},
 	},
 	plugins: [],
 }
